ca244df0712f6bdc2954da1984585a4d9f94ae85
Victor Garcia Mestre <vgmestre@gmail.com>
2022-02-01 23:08:36 +0100

    add pagination

--- graphql/types/Post.ts
+++ graphql/types/Post.ts
@@ -1,4 +1,4 @@
-import { objectType, extendType } from 'nexus';
+import { objectType, extendType, intArg, stringArg } from 'nexus';
 import { User } from './User';
 
 export const Post = objectType({
@@ -25,7 +25,8 @@ export const Post = objectType({
   },
 });
 
-export const PostsQuery = extendType({
+// without pagination:
+/* export const PostsQuery = extendType({
   type: 'Query',
   definition(t) {
     t.nonNull.list.field('posts', {
@@ -35,9 +36,108 @@ export const PostsQuery = extendType({
       },
     });
   },
-});
+}); */
 
 // '.nonNull' specifies that clients will always get a value for this field.
 // In Nexus, all "output types" (types returned by fields) are nullable by default.
 
 // '.list' specifies that this query will return a list.
+
+export const Edge = objectType({
+  name: 'Edge',
+  definition(t) {
+    t.string('cursor');
+    t.field('node', {
+      type: Post,
+    });
+  },
+});
+
+export const PageInfo = objectType({
+  name: 'PageInfo',
+  definition(t) {
+    t.string('endCursor');
+    t.boolean('hasNextPage');
+  },
+});
+
+export const Response = objectType({
+  name: 'Response',
+  definition(t) {
+    t.field('pageInfo', { type: PageInfo });
+    t.list.field('edges', {
+      type: Edge,
+    });
+  },
+});
+
+export const PostsQuery = extendType({
+  type: 'Query',
+  definition(t) {
+    t.field('posts', {
+      type: 'Response',
+      args: {
+        first: intArg(),
+        after: stringArg(),
+      },
+      async resolve(_, args, ctx) {
+        let queryResults = null;
+        if (args.after) {
+          // check if there is a cursor as the argument
+          queryResults = await ctx.prisma.post.findMany({
+            take: args.first, // the number of items to return from the database
+            skip: 1, // skip the cursor
+            cursor: {
+              id: args.after, // the cursor
+            },
+          });
+        } else {
+          // if no cursor, this means that this is the first request
+          //  and we will return the first items in the database
+          queryResults = await ctx.prisma.post.findMany({
+            take: args.first,
+          });
+        }
+        // if the initial request returns links
+        if (queryResults.length > 0) {
+          // get last element in previous result set
+          const lastLinkInResults = queryResults[queryResults.length - 1];
+          // cursor we'll return in subsequent requests
+          const myCursor = lastLinkInResults.id;
+
+          // query after the cursor to check if we have nextPage
+          const secondQueryResults = await ctx.prisma.post.findMany({
+            take: args.first,
+            cursor: {
+              id: myCursor,
+            },
+            orderBy: {
+              id: 'asc',
+            },
+          });
+          // return response
+          const result = {
+            pageInfo: {
+              endCursor: myCursor,
+              hasNextPage: secondQueryResults.length >= args.first, //if the number of items requested is greater than the response of the second query, we have another page
+            },
+            edges: queryResults.map((link) => ({
+              cursor: link.id,
+              node: link,
+            })),
+          };
+
+          return result;
+        }
+        //
+        return {
+          pageInfo: {
+            endCursor: null,
+            hasNextPage: false,
+          },
+          edges: [],
+        };
+      },
+    });
+  },
+});
